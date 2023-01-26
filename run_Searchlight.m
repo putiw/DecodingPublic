2994961233809d84b706b59a020771c318ff8422
putiwen <pw1246@nyu.edu>
2023-01-26 07:08:35 -0500

    check if results file already exist

--- run_Searchlight.m
+++ run_Searchlight.m
@@ -1,21 +1,24 @@
-clear all;clc;close all; restoredefaultpath;
-addpath(genpath([pwd '/helper_functions']));
-bidsDir = '/Volumes/Vision/MRI/DecodingPublic';
-fsDir = '/Applications/freesurfer/7.2.0';
-gitDir = '~/Documents/GitHub';
-set_up(bidsDir,gitDir,fsDir)
+%% set up 
 
-tic
+clear all;clc;close all; 
+restoredefaultpath;
+addpath(genpath([pwd '/helper_functions'])); % path to helper functions
+bidsDir = '/Volumes/Vision/MRI/DecodingPublic'; % path to bids
+fsDir = '/Applications/freesurfer/7.2.0'; % path to freesurfer
+gitDir = '~/Documents/GitHub'; % path to github
+set_up(bidsDir,gitDir,fsDir) % set up path and dependencies
+
 %% find searchlight index
 
-% load fsaverage6 .inflated surf
+% load fsaverage6 .inflated surf to get xyz coordinates
 surfL = fullfile([bidsDir '/derivatives/freesurfer/fsaverage6/surf/lh.inflated']);
 surfL = fs_read_surf(surfL);
 surfR = fullfile([bidsDir '/derivatives/freesurfer/fsaverage6/surf/rh.inflated']);
 surfR = fs_read_surf(surfR);
 num = size(surfL.coord,2);
-% find what is inside for each searchlight
-idx = zeros(num,100,2);
+
+% find what is inside for each searchlight using the xyz coordinates
+idx = zeros(num,100,2); %for each vertex, list the closest 100 vertices, for each hemi
 for iVer = 1:num
     distance = sqrt(sum((surfL.coord - surfL.coord(:,iVer)).^2));
     [~,idx(iVer,:,1)] = mink(distance,100);
@@ -23,78 +26,98 @@ for iVer = 1:num
     [~,idx(iVer,:,2)] = mink(distance,100);
 end
 
-%%
-subject = {'sub-0201','sub-0202','sub-0204','sub-0205','sub-0206','sub-0228','sub-0229','sub-0248','sub-0903'};     % subject ID
-ses =   {'03','04'}; %{'03','04'}{'01','02'}
-run = [1:10]';
-hemi = {'L','R'};
+%% Run searchlight
 
-for iSub = 1:9
-    sub = subject{iSub};
+% mkdir
+if ~exist([bidsDir,'/derivatives/dataMat'],'dir')
+    mkdir([bidsDir,'/derivatives/dataMat']);
+end
+if ~exist([bidsDir,'/derivatives/resultMat'],'dir')
+    mkdir([bidsDir,'/derivatives/resultMat']);
+end
+
+% set up params
+subject = {'sub-0201','sub-0202','sub-0204','sub-0205','sub-0206','sub-0228','sub-0229','sub-0248','sub-0903'};     % subject ID
+session = {'01','02','03','04'}; % which session 
+run = [1:10]'; % which run 
+hemi = {'L','R'}; % which hemi
+nRep = 1000; % bootstraps repeats
+nTest = 1; % percetange of data set as testing trials
+nTrial = 160;  % number of trials
+nDir = 8; % number of directions
+nScans = nTrial/nDir; % number of scans
+labelTrain = sort(repmat(repelem(1:8,1)',nScans-nTest,1)); % training dataset stimulus label
+labelTest = repmat(repelem(1:8,1)',nTest,1); % testing dataset stimulus label
+
+
+for iSub = 1:9 % looping through subjects
     
-    %% load surf data
-    
-    samples = load_surf(bidsDir,sub,ses,run,'fsaverage6',hemi,'fft');
-    label = [repmat([[4:8 1:3]';[5:-1:1 8:-1:6]'],10,1) repmat([8 1:7 1 8:-1:2]',10,1)];
-    
-    if ~exist([bidsDir,'/derivatives/dataMat'],'dir')
-        mkdir([bidsDir,'/derivatives/dataMat']);
-    end
-    savedata = fullfile(bidsDir,'/derivatives/dataMat',[sub,'-ses-' ses{:} '-fsaverage6.mat']);
-    save(savedata,'samples','label');
-    
-    %% MATLAB Classify
-    
-    nRep = 1000; % bootstraps repeats
-    test_n = 1; % percetange of data set as testing trials
-    
-    trial_n = 160;  % number of trials
-    dir_n = 8; % number of directions
-    nScans = trial_n/dir_n; % number of scans
-    
-    Training_group = sort(repmat(repelem(1:8,1)',nScans-test_n,1)); % training dataset stimulus label
-    Testing_group = repmat(repelem(1:8,1)',test_n,1); % testing dataset stimulus label
-    
-    pc = zeros(nRep,num,2); % percentage correct
-    
-    for iHemi = 1:2
+    for iSes = 1:2
         
-        parfor ii = 1:num
+        ses =  session(iSes*2-1:iSes*2);
+        
+        % load surf data        
+        dataFile = fullfile(bidsDir,'/derivatives/dataMat',[subject{iSub},'-ses-' ses{:} '-fsaverage6.mat']);
+        
+        if ~exist(dataFile,'file') % if cleaned .mat file doesn't exist, load from gifti file
             
-            data = samples(:,idx(ii,:,iHemi),iHemi); % 160 (trials) x 100 (vertices)
+            % samples -> 160 (trials) x 40962 (vertices) x 2 (hemi)
+            samples = load_surf(bidsDir,subject{iSub},ses,run,'fsaverage6',hemi,'fft');
+            % stimulus label -> 160 (trials) x 2 (hemi)
+            label = [repmat([[4:8 1:3]';[5:-1:1 8:-1:6]'],10,1) repmat([8 1:7 1 8:-1:2]',10,1)];
             
-            for kk = 1:nRep
-                
-                Training_data = [];
-                Testing_data = [];
-                
-                for mm = 1:dir_n
-                    train_run = 1:nScans; % run index
-                    test_run = randperm(nScans,test_n); %random select (test_n) trial for each direction as testing trial index (without replacement)
-                    train_run(test_run) = []; % set the rest as the training trial index
-                    dir_idx = find(label(:,iHemi)==mm); % find trial index for this direction
-                    Testing_data = [Testing_data; data(dir_idx(test_run),:)];
-                    Training_data = [Training_data; data(dir_idx(train_run),:)];
-                end
-                
-                decoded_group = classify(Testing_data,Training_data,Training_group,'diaglinear');
-                pc(kk,ii,iHemi) = length(find(decoded_group == Testing_group))/length(Testing_group); % percentage correct
-                [sub iHemi ii]
-            end
+            disp(['saving ' subject{iSub},'-ses-' ses{:} '-fsaverage6.mat to ' bidsDir,'/derivatives/dataMat/'])
+            save(dataFile,'samples','label');
+            
+        else  % load cleaned .mat file if it exists to save time
+            disp(['data file exists, loading ' subject{iSub},'-ses-' ses{:} '-fsaverage6.mat from ' bidsDir,'/derivatives/dataMat/'])
+            load(dataFile);
         end
         
+        
+        % MATLAB Classify                    
+        resultFile = fullfile(bidsDir,'/derivatives/dataMat',[subject{iSub},'-ses-' ses{:} '-fsaverage6.mat']);
+        
+        if exist(resultFile,'file')
+            prompt = sprintf('This file already exists:\n%s\nDo you want to overwrite it?', resultFile);
+            question = 'Overwrite?';
+            answer = questdlg(prompt, question, 'Yes','No','Stop','Yes');
+            switch answer
+                case 'yes' % re-analysis the data and overwrite the results 
+                    pc = zeros(nRep,num,2); % percentage correct. bootsrap repeats x vertices x 2 hemi                   
+                    for iHemi = 1:2                      
+                        parfor ii = 1:num  
+                            
+                            data = samples(:,idx(ii,:,iHemi),iHemi); % 160 (trials) x 100 (vertices)   
+                            
+                            for kk = 1:nRep     
+                                
+                                dataTrain = [];
+                                dataTest = [];       
+                                
+                                for mm = 1:nDir
+                                    train_run = 1:nScans; % run index
+                                    test_run = randperm(nScans,nTest); %random select (nTest) trial for each direction as testing trial index (without replacement)
+                                    train_run(test_run) = []; % set the rest as the training trial index
+                                    whichTrial = find(label(:,iHemi)==mm); % find trial index for this direction
+                                    dataTest = [dataTest; data(whichTrial(test_run),:)];
+                                    dataTrain = [dataTrain; data(whichTrial(train_run),:)];
+                                end
+                                
+                                decoded_group = classify(dataTest,dataTrain,labelTrain,'diaglinear');
+                                pc(kk,ii,iHemi) = length(find(decoded_group == labelTest))/length(labelTest); % percentage correct
+                            end
+                        end                        
+                    end
+                    
+                    acc = nanmean(pc,1);
+                    acc = cat(1,acc(:));                    
+                    save(resultFile,'acc');
+                    
+                case 'No' % result file already exists, skip this subject 
+                case 'Stop'
+                   return 
+            end
+        end                
     end
-    
-    acc = nanmean(pc,1);
-    acc = cat(1,acc(:));
-    
-    if ~exist([bidsDir,'/derivatives/resultMat'],'dir')
-        mkdir([bidsDir,'/derivatives/resultMat']);
-    end
-    savedata = fullfile(bidsDir,'/derivatives/resultMat',[sub,'_ses-' ses{:} '_searchlight_fsaverage6.mat']);
-    save(savedata,'acc');
-    
-    
-    
 end
-toc
--- run_TAFKAP.m
+++ run_TAFKAP.m
@@ -1,99 +1,113 @@
+%% set up
 clear all; close all; clc
-
-% Dependencies
-restoredefaultpath
+restoredefaultpath;
 addpath(genpath([pwd '/helper_functions']));
 addpath(genpath('~/Documents/GitHub/TAFKAP')); % https://github.com/Rokers/TAFKAP
-
 bidsDir = '/Volumes/Vision/MRI/DecodingPublic/';
-
-subject = {'sub-0201','sub-0202','sub-0204','sub-0205','sub-0206','sub-0228','sub-0229','sub-0248','sub-0903'};                                          % subject ID
-
+subject = {'sub-0201','sub-0202','sub-0204','sub-0205','sub-0206','sub-0228','sub-0229','sub-0248','sub-0903'};    % subject ID
 session = {'01','02','03','04'};
 run = [1:10]';
-roi =  {'V1','hMT'};
+roi = {'V1','V2','V3','V3A','V3B','hV4','LO1','LO2','hMT','MST','IPS0','IPS1','IPS2','IPS3','IPS4','IPS5','VO1','VO2','SPL1','PHC1','PHC2','FEF'};
 nFolds = 32; % Set to multiple of number of processing cores
-maxvoxel = 200;
-
+maxvoxel = inf; %max voxel used in the analysis
+if ~exist([bidsDir,'derivatives/dataMat'],'dir')
+    mkdir([bidsDir,'derivatives/dataMat'])
+end
+if ~exist([bidsDir,'derivatives/resultsMat'],'dir')
+    mkdir([bidsDir,'derivatives/resultsMat'])
+end
+%% TAFKAP
 for iSub = 1:9
-    
-    sub = subject{iSub};
-    
     for iSes = 1:2
+        ses =  session(iSes*2-1:iSes*2); % session ID
+        params = SetupTAFKAP(); % set up parameters
         
-        ses =  session(iSes*2-1:iSes*2);
-        %roi = {'V1','V2','V3','V3A','V3B','hV4','LO1','LO2','hMT','MST','IPS0','IPS1','IPS2','IPS3','IPS4','IPS5','VO1','VO2','SPL1','PHC1','PHC2','FEF'};
-        params = SetupTAFKAP();
-        [allVoxelSample,stim_label] = load_vol(BASE,sub,ses,run,roi,params);
-        %
+        % load data
+        dataFile = fullfile(bidsDir,'derivatives/dataMat',[subject{iSub},'-ses-' session{iSes*2-1:iSes*2} '-volumn.mat']);
+        if ~exist(dataFile,'file') % if cleaned .mat file doesn't exist, load from gifti file
+            [allVoxelSample,stim_label] = load_vol(bidsDir,subject{iSub},ses,run,roi,params);
+            disp(['saving ' subject{iSub},'-ses-' session{iSes*2-1:iSes*2} '-volumn.mat to ' bidsDir,'/derivatives/dataMat/'])
+            save(dataFile,'allVoxelSample','stim_label','roi');
+        else  % load cleaned .mat file if it exists to save time
+            disp(['data file exists, loading ' subject{iSub},'-ses-' session{iSes*2-1:iSes*2} '-volumn.mat from ' bidsDir,'/derivatives/dataMat/'])
+            load(dataFile);
+        end
+        
+        % load data if using diffferent num of voxels
         samples = cell(nFolds,numel(roi,1));
-        voxelsize = numel(roi,1);
-        
-        for i = 1:numel(roi)
-            voxelsize(i,1)=size(allVoxelSample{i},2);
+        voxelsize = zeros(numel(roi),1);
+        % random select *maxvoxel* number voxels from each ROIs for each of the
+        % cross-validatin fold
+        for iRoi = 1:numel(roi)
+            voxelsize(iRoi,1)=size(allVoxelSample{iRoi},2);
             for iF = 1:nFolds
-                if voxelsize(i,1) <= maxvoxel
-                    samples{iF,i} = allVoxelSample{i};
+                if voxelsize(iRoi,1) <= maxvoxel % if a given ROI has less than *maxvoxel* number of voxels, then use all the entire ROI
+                    samples{iF,iRoi} = allVoxelSample{iRoi};
                 else
-                    samples{iF,i} = allVoxelSample{i}(:,randperm(voxelsize(i,1),maxvoxel));
+                    samples{iF,iRoi} = allVoxelSample{iRoi}(:,randperm(voxelsize(iRoi,1),maxvoxel));
                 end
             end
         end
         
-        if ~exist([bidsDir,'derivatives/dataMat'],'dir')
-            mkdir([bidsDir,'derivatives/dataMat'])
-        end
-        savedata = fullfile(bidsDir,'derivatives/dataMat',[sub,'-ses-' session{iSes*2-1:iSes*2} '-volumn.mat']);
-        save(savedata,'samples','stim_label','roi','voxelsize');
-        
         %% Run TAFKAP
         
-        % Setup design (parameters)
-        nScans = length(ses)*length(run); % scans per subject
-        nDirs = 8; % motion directions
-        params.stimval = stim_label;
-        params.runNs = reshape(repmat(1:nScans,nDirs,1),1,[])'; %trainruns; % stimulus block/run
-        
-        pre = cell(nFolds,1);  % Preallocate
-        for ii = 1:nFolds
-            p{ii} = params;
-            c = cvpartition(params.stimval, 'Holdout', 0.1); % stratify by motion direction, but not scan
-            p{ii}.train_trials = c.training;
-            p{ii}.test_trials = c.test;
-            pre{ii} = params.stimval(c.test);
-        end
-        
-        % Run TAFKAP
-        ests = cell(numel(roi),1); % Preallocate
-        uncs = cell(numel(roi),1); % Preallocate
-        pres = cell(numel(roi),1); % Preallocate
-        for whichRoi = 1:numel(roi)
-            parfor ii = 1:nFolds
-                ii
-                rng(ii);% To counter the effects of TAFKAP_Decode setting the system rand seed to const. This was an EXTRAORDINARLY hard bug to find.
-                [est{ii}, unc{ii}, liks{ii}, hypers{ii}] = TAFKAP_Decode(samples{ii,whichRoi}, p{ii});
+        resultFile = fullfile(bidsDir,'derivatives/resultsMat',[subject{iSub},'-ses-' ses{:} '-TAFKAP.mat']);
+        if exist(resultFile,'file')
+            prompt = sprintf('This file already exists:\n%s\nDo you want to overwrite it?', resultFile);
+            question = 'Overwrite?';
+            answer = questdlg(prompt, question, 'Yes','No','Yes');
+            switch answer
+                case 'yes' % re-analysis the data and overwrite the results
+                    % Setup design (parameters)
+                    nScans = length(ses)*length(run); % scans per subject
+                    nDirs = 8; % motion directions
+                    params.stimval = stim_label;
+                    params.runNs = reshape(repmat(1:nScans,nDirs,1),1,[])'; %trainruns; % stimulus block/run
+                    
+                    pre = cell(nFolds,1);  % Preallocate
+                    for ii = 1:nFolds
+                        p{ii} = params;
+                        c = cvpartition(params.stimval, 'Holdout', 0.1); % stratify by motion direction, but not scan
+                        p{ii}.train_trials = c.training;
+                        p{ii}.test_trials = c.test;
+                        pre{ii} = params.stimval(c.test);
+                    end
+                    
+                    % Run TAFKAP
+                    ests = cell(numel(roi),1); % Preallocate
+                    uncs = cell(numel(roi),1); % Preallocate
+                    pres = cell(numel(roi),1); % Preallocate
+                    for whichRoi = 1:numel(roi)
+                        parfor ii = 1:nFolds
+                            ii
+                            rng(ii);% To counter the effects of TAFKAP_Decode setting the system rand seed to const. This was an EXTRAORDINARLY hard bug to find.
+                            [est{ii}, unc{ii}, liks{ii}, hypers{ii}] = TAFKAP_Decode(samples{ii,whichRoi}, p{ii});
+                        end
+                        ests{whichRoi} = cell2mat(est');
+                        uncs{whichRoi} = cell2mat(unc');
+                        pres{whichRoi} = cell2mat(pre);
+                    end
+                    % Calculate performance
+                    params.subjects = subject{iSub};
+                    saveresult = cell(numel(roi),1);
+                    for whichRoi = 1:numel(roi)
+                        % conmat = confusionmat(categorical(ceil(pres{rr}/22.5)),categorical(ceil(ests{rr}/22.5))); % continuous data
+                        conmat = confusionmat(categorical(pres{whichRoi}),categorical(ests{whichRoi})); % categorical data
+                        conmat = 100.*conmat./sum(conmat,2); % convert to % accuracy
+                        conmat = [conmat; conmat(1,:)]; % wrap matrix
+                        conmat = [conmat, conmat(:,1)];
+                        saveresult{whichRoi} = conmat';
+                        disp(['Classification performance ' roi{whichRoi} ': ' num2str(100.*mean(pres{whichRoi}==ests{whichRoi})) '%'])
+                    end
+                    
+                    save(resultFile,'saveresult','ests','uncs','pres','roi','voxelsize');
+                    
+                case 'No' % result file already exists, print results
+                    load(resultFile);
+                    for whichRoi = 1:numel(roi)
+                        disp(['Classification performance ' roi{whichRoi} ': ' num2str(100.*mean(pres{whichRoi}==ests{whichRoi})) '%'])
+                    end
             end
-            ests{whichRoi} = cell2mat(est');
-            uncs{whichRoi} = cell2mat(unc');
-            pres{whichRoi} = cell2mat(pre);
         end
-        toc
-        %% Calculate performance
-        params.subjects = sub;
-        saveresult = cell(numel(roi),1);
-        for whichRoi = 1:numel(roi)
-            % conmat = confusionmat(categorical(ceil(pres{rr}/22.5)),categorical(ceil(ests{rr}/22.5))); % continuous data
-            conmat = confusionmat(categorical(pres{whichRoi}),categorical(ests{whichRoi})); % categorical data
-            conmat = 100.*conmat./sum(conmat,2); % convert to % accuracy
-            conmat = [conmat; conmat(1,:)]; % wrap matrix
-            conmat = [conmat, conmat(:,1)];
-            saveresult{whichRoi} = conmat';
-            disp(['Classification performance ' roi{whichRoi} ': ' num2str(100.*mean(pres{whichRoi}==ests{whichRoi})) '%'])
-        end
-        if ~exist([bidsDir,'derivatives/resultsMat'],'dir')
-            mkdir([bidsDir,'derivatives/resultsMat'])
-        end
-        f = fullfile(bidsDir,'derivatives/resultsMat',[sub,'-ses-' ses{:} '-TAFKAP.mat']);
-        save(f,'saveresult','ests','uncs','pres','roi','voxelsize');
     end
 end
